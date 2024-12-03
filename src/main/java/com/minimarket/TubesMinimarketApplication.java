f5e3c3d7b8e54383f92fcf0a5d7ff1a936442c3b
RUSDI00 <149083939+RUSDI00@users.noreply.github.com>
2024-12-03 12:01:48 +0700

    Kerangka

---
+++ src/main/java/com/minimarket/TubesMinimarketApplication.java
@@ -0,0 +1,13 @@
+package com.minimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubesMinimarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubesMinimarketApplication.class, args);
+	}
+
+}
